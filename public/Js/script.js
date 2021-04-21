fc7bcb6841c7df3c31415b88b42c288b319e8f85
Archit garg <architgarg603@gmail.com>
2021-04-21 15:00:54 +0530

    added dropdown

--- public/Js/script.js
+++ public/Js/script.js
@@ -14,30 +14,45 @@ let aboutDiv = document.querySelector(".about")
 let graphDiv = document.querySelector(".graph")
 let state = document.querySelector("#state");
 let word = document.querySelector("#Search")
+let latLabel = document.querySelector(".lat")
+let longLabel = document.querySelector(".long")
 // stats page
 
 window.addEventListener("load", function () {
     // burgerNavBar.addEventListener("click", burgerNav);
 
+    state.addEventListener("change", function () {
+        let option = state.value;
+
+        if (option == "Select") {
+            lat.style.display = "none"
+            long.style.display = "none"
+            latLabel.style.display = "none"
+            longLabel.style.display = "none"
+
+        } else if (option == "Other") {
+            lat.style.display = "flex"
+            long.style.display = "flex"
+            longLabel.style.display = "flex"
+            latLabel.style.display = "flex"
+        }
+    })
+
     submit.addEventListener("click", async function (e) {
         try {
 
             let option = state.value;
-           
-            if (option == "Select") {
-                lat.style.display = "none"
-                long.style.display = "none"
 
+            if (option == "Select") {
+           
             } else if (option == "Other") {
-                lat.style.display = "flex"
-                long.style.display = "flex"
                 if (lat.value && long.value) {
                     e.preventDefault();
                     let words = word.value.split(" ");
                     let formData = new FormData();
                     formData.append("city1", lat.value);
                     formData.append("city2", long.value);
-                    formData.append("words",words);
+                    formData.append("words", words);
 
                     load.style.display = "none";
                     loadingAnime.style.display = "flex";
@@ -98,74 +113,71 @@ window.addEventListener("load", function () {
 
             } else {
                 e.preventDefault();
-                
-                lat.style.display = "none"
-                long.style.display = "none"
 
                 let cord = option.split(" ");
                 let words = word.value.split(" ");
                 let formData = new FormData();
-                    formData.append("city1", cord[0]);
-                    formData.append("city2", cord[1]);
-                    formData.append("words",words);
+                formData.append("city1", cord[0]);
+                formData.append("city2", cord[1]);
+                formData.append("words", words);
 
-                    load.style.display = "none";
-                    loadingAnime.style.display = "flex";
-                    mess.innerHTML = "";
-                    let data = await axios.post("https://avekshaml.herokuapp.com/", formData)
-                    let cloudURL = data.data.url1;
-                    let graphURL = data.data.url2;
-                    data = data.data.row_data;
-                    let arr = []
+                load.style.display = "none";
+                loadingAnime.style.display = "flex";
+                mess.innerHTML = "";
+                let data = await axios.post("https://avekshaml.herokuapp.com/", formData)
+                let cloudURL = data.data.url1;
+                let graphURL = data.data.url2;
+                data = data.data.row_data;
+                let arr = []
+
+                for (let i = 0; i < data.length; i++) {
+                    let obj = {
+                        "atag": data[i][0],
+                        "location": data[i][1],
+                        "hash": data[i][4],
+                        "tweet": data[i][3],
+                        "pol": data[i][7]
+                    }
+                    arr.push(obj);
+                }
+
+                arr.sort((a, b) => (a.pol > b.pol) ? 1 : ((b.pol > a.pol) ? -1 : 0))
+                let wrap = document.createElement("div");
+                if (lists != null) {
 
                     for (let i = 0; i < data.length; i++) {
-                        let obj = {
-                            "atag": data[i][0],
-                            "location": data[i][1],
-                            "hash": data[i][4],
-                            "tweet": data[i][3],
-                            "pol": data[i][7]
-                        }
-                        arr.push(obj);
+                        let pt = document.createElement("div");
+                        pt.classList.add("pt");
+                        let name = document.createElement("h3");
+                        let atag = document.createElement("a");
+                        atag.innerHTML = arr[i].atag;
+                        atag.setAttribute("target", "_blank");
+                        atag.setAttribute("href", `https://twitter.com/${arr[i].atag}`)
+                        name.appendChild(atag)
+                        let location = document.createElement("h3");
+                        location.innerHTML = arr[i].location;
+                        let hash = document.createElement("h3");
+                        hash.innerHTML = arr[i].hash;
+                        let tweet = document.createElement("h3");
+                        tweet.innerHTML = arr[i].tweet;
+                        pt.appendChild(name);
+                        pt.appendChild(location);
+                        pt.appendChild(hash)
+                        pt.appendChild(tweet);
+                        wrap.appendChild(pt);
                     }
 
-                    arr.sort((a, b) => (a.pol > b.pol) ? 1 : ((b.pol > a.pol) ? -1 : 0))
-                    let wrap = document.createElement("div");
-                    if (lists != null) {
+                    lists.append(wrap);
+                }
+                aboutDiv.style.backgroundImage = `url(https://avekshaml.herokuapp.com/${cloudURL.substring(1)})`
+                graphDiv.style.backgroundImage = `url(https://avekshaml.herokuapp.com/${graphURL.substring(1)})`
+                loadingAnime.style.display = "none";
+                block.style.display = "flex";
 
-                        for (let i = 0; i < data.length; i++) {
-                            let pt = document.createElement("div");
-                            pt.classList.add("pt");
-                            let name = document.createElement("h3");
-                            let atag = document.createElement("a");
-                            atag.innerHTML = arr[i].atag;
-                            atag.setAttribute("target", "_blank");
-                            atag.setAttribute("href", `https://twitter.com/${arr[i].atag}`)
-                            name.appendChild(atag)
-                            let location = document.createElement("h3");
-                            location.innerHTML = arr[i].location;
-                            let hash = document.createElement("h3");
-                            hash.innerHTML = arr[i].hash;
-                            let tweet = document.createElement("h3");
-                            tweet.innerHTML = arr[i].tweet;
-                            pt.appendChild(name);
-                            pt.appendChild(location);
-                            pt.appendChild(hash)
-                            pt.appendChild(tweet);
-                            wrap.appendChild(pt);
-                        }
 
-                        lists.append(wrap);
-                    }
-                    aboutDiv.style.backgroundImage = `url(https://avekshaml.herokuapp.com/${cloudURL.substring(1)})`
-                    graphDiv.style.backgroundImage = `url(https://avekshaml.herokuapp.com/${graphURL.substring(1)})`
-                    loadingAnime.style.display = "none";
-                    block.style.display = "flex";
-
-                
 
             }
-         
+
 
         } catch (err) {
             load.style.display = "flex";
